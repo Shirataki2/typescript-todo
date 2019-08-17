983a99d096c1add73dcdb587f390e07596b9b52f
Shirataki2 <ahahah114514@gmail.com>
2019-08-18 00:28:12 +0900

    Init

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 md8 lg4 class="text-center">
+        <span class="display-4">Todo</span>      
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Index extends Vue {}
+</script>
